8ac65777809ed4280de86b9d7b15a8eee4c1821a
lg0805 <lg0805@qq.com>
2017-08-13 12:41:21 +0800

    ajax

---
+++ login.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<script src="http://ajax.microsoft.com/ajax/jquery/jquery-1.4.min.js"></script>
+	<script src="login_js.js"></script>
+</head>
+<body>
+	<h1>Login</h1>
+	<p id="results">abc </p>
+
+	<form action="login_ajax.php" method="post" id="login">
+		<p id="emailP">
+			<label for="email">Email Address:</label> 
+			<input type="text" name="email" id="email" />
+			<span class="errorMessage" id="emailError">Please enter your email address!</span>
+		</p>
+		<p id="passwordP">
+			<label for="password">Password:</label>
+			<input type="password" name="password" id="password" />
+			<span class="errorMessage" id="passwordError">Please enter your password!</span>
+		</p>
+		<p><input type="submit" value="Login!" id="submit" /></p>
+
+	</form>
+</body>
+</html>
---
+++ login_js.js
@@ -0,0 +1,83 @@
+$(function(){
+
+	$('.errorMessage').hide();
+
+	$('#login').submit(function(){
+
+		var email, password;
+
+		if ($('#email').val().length >= 6) {
+
+			email = $('#email').val();
+
+			$('#emailP').removeClass('error');
+			$('#emailError').hide();
+		} else {
+			$('#emailP').addClass('error');
+			$('#emailError').show();
+		};
+
+		if ($('#password').val().length > 0) {
+
+			password = $('#password').val();
+
+			$('#passwordP').removeClass('error');
+			$('#passwordError').hide();
+		} else {
+
+			$('#passwordP').addClass('error');
+			$('#passwordError').show();
+		};
+
+		// if appropriate, perform the Ajax request
+		// 如果合适，执行Ajax请求
+		
+		if (email && password) {
+
+			// Create an object for the form request
+			// 为表单请求创建一个对象
+			var data = new Object;
+
+			data.email = email;
+			data.password = password;
+
+			// Create an object of Ajax options
+			// 创建Ajax选项的对象
+			var options = new Object;
+
+			options.data = data;
+			options.dataType = "text";
+			options.type = 'get';
+			options.success = function(response){
+
+				if (response == 'CORRECT') {
+
+					$('#login').hide();
+
+					$('#results').removeClass('error');
+					$('#results').text('You are now logged in!');
+				} else if(response == "INCORRECT") {
+				
+					$('#results').text('The submitted credentials do not match those in file!');
+					$('#results').addClass('error');
+				} else if(response == 'INCOMPLETE'){
+					$('#results').text('Please provide an email address and a password!');
+					$('#results').addClass('error');
+				} else if(response == 'INVALID_EMAIL'){
+					$('#results').text('Please provide your email address!');
+					$('#results').addClass('error');
+				}
+			} // End of success
+
+			options.url = 'login_ajax.php';
+
+			$.ajax(options);
+					
+		}; 
+		
+
+		return false;
+		// alert('ok');
+	});
+	
+})
